f056f9cee44d603cd2d5cac4918026febc0b0475
syotakokichi <121482220+syotakokichi@users.noreply.github.com>
2023-06-19 04:06:22 +0900

    Delete week9-10/5_RealWorld directory

--- week9-10/5_RealWorld/blog_app/app/models/tag.rb
+++
@@ -1,4 +0,0 @@
-class Tag < ApplicationRecord
-  has_many :article_tags
-  has_many :articles, through: :article_tags
-end
--- week9-10/5_RealWorld/blog_app/config/routes.rb
+++
@@ -1,5 +0,0 @@
-Rails.application.routes.draw do
-  namespace :api do
-    resources :articles, only: [:create, :show, :update, :destroy]
-  end
-end
